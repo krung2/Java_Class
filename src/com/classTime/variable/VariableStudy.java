e4d8e2bf8cf9a88ba84cb0c958cd637ac5ee77bb
krung2 <jungbin4337@dgsw.hs.kr>
2021-03-24 13:31:57 +0900

    Add: Variablesutdy Class

---
+++ src/com/classTime/variable/VariableStudy.java
@@ -0,0 +1,19 @@
+package com.classTime.variable;
+
+public class VariableStudy {
+
+  String name;
+
+  public void method (String param) {
+    int local;
+
+  }
+
+  public static void main(String[] args) {
+    VariableStudy study1 = new VariableStudy();
+    VariableStudy study2 = new VariableStudy();
+
+    study1.name = "홍길동";
+
+  }
+}
