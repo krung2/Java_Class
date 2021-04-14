36d1e8d38061c53fb2deaeca4c540c2dc6c402d5
krung2 <jungbin4337@dgsw.hs.kr>
2021-04-14 14:57:50 +0900

    Add: Insert Sort algorithim

---
+++ src/com/classTime/array/InsertSort.java
@@ -0,0 +1,36 @@
+package com.classTime.array;
+
+public class InsertSort extends Sort {
+
+  @Override
+  public void sort() {
+    for (int i = 1;i < array.length; i ++) {
+      int key = array[i];
+      int j;
+
+      for (j = i - 1; j >= 0 && array[j] > key;j --) {
+        array[j + 1] = array[j];
+      }
+
+      array[j + 1] = key;
+    }
+  }
+
+  public static void main(String[] args) {
+    Sort sort = new InsertSort();
+
+    sort.fillValues();
+
+    System.out.println(" 정렬 전");
+    sort.printArray();
+
+    long before = System.currentTimeMillis();
+    sort.sort();
+    long after = System.currentTimeMillis();
+
+    System.out.println("\n\n 정렬 후");
+    sort.printArray();
+
+    System.out.println("\n경과 시간 : " + (after - before) + "초");
+  }
+}
---
+++ src/com/classTime/array/Sort.java
@@ -0,0 +1,27 @@
+package com.classTime.array;
+
+import java.util.Random;
+
+public abstract class Sort {
+
+  private static final int SIZE = 100;
+
+  protected int[] array = new int[SIZE];
+
+  public void fillValues() {
+    Random random = new Random();
+
+    for (int i = 0;i < array.length; i ++) {
+      array[i] = random.nextInt(10000);
+    }
+  }
+
+  public void printArray() {
+      for (int i = 0;i < array.length; i ++) {
+        System.out.println(i + " : " + array[i]);
+      }
+  }
+
+  public abstract void sort();
+
+}
