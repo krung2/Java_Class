8c880d560279ecbdaf26e4b75ec6e1fa951dc57e
krung2 <jungbin4337@dgsw.hs.kr>
2021-05-12 13:53:25 +0900

    Add: fileIO

--- src/com/classTime/file/FileStudy.java
+++ src/com/classTime/file/FileStudy.java
@@ -1,14 +1,47 @@
 package com.classTime.file;
 
 import java.io.File;
+import java.util.Date;
 
 public class FileStudy {
 
   public static void main(String[] args) {
 
-    File file = new File("/Users/krung2/Desktop/java/abc.txt");
+    String baseURL = "/Users/krung2/Desktop/java/file/";
 
-    boolean exist = file.exists();
-    System.out.println("exist : " + exist);
+    try {
+      File file = new File(baseURL + "abc.txt");
+
+      boolean exist = file.exists();
+      System.out.println("exist : " + exist);
+
+      file = new File(baseURL + "가나다.txt");
+      boolean result = file.createNewFile();
+      System.out.println("createNewFile : " + result);
+
+      System.out.println("deleteFile : " + file.delete() + "\n");
+
+      File dir = new File(baseURL + "sub");
+      result = dir.mkdir();
+      System.out.println("mkdir : " + result);
+      System.out.println("rm : " + dir.delete() + "\n");
+
+      File hello = new File(baseURL + "hello");
+      System.out.println("isFile : " + hello.isFile());
+      System.out.println("isDirectory : " + hello.isDirectory());
+
+      System.out.println("getName : " + hello.getName());
+      System.out.println("getName : " + hello.getAbsolutePath());
+      System.out.println("length : " + hello.length());
+      System.out.println("lastModified : " + hello.lastModified());
+
+      long lastModified = hello.lastModified();
+      Date date = new Date(lastModified);
+      System.out.println("lastModified : " + date.toString());
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+    }
   }
 }
