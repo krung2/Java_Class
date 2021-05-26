5d961352a8b18adec5b30db96aed8ed82155a8c1
krung2 <jungbin4337@dgsw.hs.kr>
2021-05-26 14:12:53 +0900

    Add: cd && nowdir

--- src/com/classTime/file/commandLine/Command.java
+++ src/com/classTime/file/commandLine/Command.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 
 public class Command {
 
-  private String dirpath = "/Users/krung2";
+  private String dirpath = "/Users/krung2/";
+
+  public void nowDir () throws Exception {
+
+    System.out.print(this.dirpath + " >  ");
+  }
 
   public void ls () throws Exception {
     File dir = new File(this.dirpath);
@@ -13,6 +18,24 @@ public class Command {
 
     for (File file: files) {
       System.out.println(file);
+    }
+  }
+
+  public void cd (String path) throws Exception{
+    if (path.equals("..")) {
+
+      System.out.println(this.dirpath.substring((this.dirpath.lastIndexOf("/"))));
+      this.dirpath = this.dirpath.substring(0, this.dirpath.lastIndexOf("/"));
+      return;
+    }
+
+    String newPath = this.dirpath + path + "/";
+    File dir = new File(newPath);
+
+    if (dir.isDirectory()) {
+      this.dirpath = newPath;
+    } else {
+      System.out.println("없는 경로입니다");
     }
   }
 
@@ -25,11 +48,17 @@ public class Command {
 
       while (true) {
 
-        String command = scanner.nextLine();
+        commandLine.nowDir();
 
-        switch (command) {
+        String command = scanner.nextLine();
+        String[] commandSplit = command.split(" ");
+
+        switch (commandSplit[0]) {
           case "ls":
             commandLine.ls();
+            break;
+          case "cd":
+            commandLine.cd(commandSplit[1]);
             break;
           default :
             break;
