c9e074ea93f723ed15c65ddb5afb0d8e0fc63c7f
krung2 <jungbin4337@dgsw.hs.kr>
2021-06-01 13:43:21 +0900

    Add: libs jdbc

---
+++ src/com/classTime/jdbc/Jdbc.java
@@ -0,0 +1,14 @@
+package com.classTime.jdbc;
+
+public class Jdbc {
+
+  public static void main(String[] args) {
+
+    try {
+      Class.forName("org.mariadb.jdbc.Driver");
+      System.out.println("Success!");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
