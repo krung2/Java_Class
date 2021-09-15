a60c641e70ce8034320bfe03191b38a74a41846b
krung2 <jungbin4337@dgsw.hs.kr>
2021-09-15 17:25:20 +0900

    Add

---
+++ src/kr/hs/dgsw/javaClass/server/nioNonBlockingServer/NioNonBlockingServer.java
@@ -0,0 +1,48 @@
+package kr.hs.dgsw.javaClass.server.nioNonBlockingServer;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+public class NioNonBlockingServer {
+
+  private ServerSocketChannel serverSocketChannel;
+
+  public void start (int port) throws Exception {
+    Selector selector = Selector.open();
+
+    serverSocketChannel = ServerSocketChannel.open();
+    serverSocketChannel.bind(new InetSocketAddress(port));
+    serverSocketChannel.configureBlocking(false);
+
+    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+    while (true) {
+      int count = selector.select();
+      Set<SelectionKey> keys = selector.keys();
+
+      for (SelectionKey key: keys) {
+        if (key.isAcceptable()) {
+          ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+          SocketChannel socketChannel = channel.accept();
+        } else if (key.isReadable()) {
+
+        } else if (key.isWritable()) {
+
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    try {
+      NioNonBlockingServer server = new NioNonBlockingServer();
+      server.start(3001);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
